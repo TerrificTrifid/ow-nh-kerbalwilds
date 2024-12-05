99a4feef14654aaf59197023e64da02ea94c0716
TerrificTrifid <99054745+TerrificTrifid@users.noreply.github.com>
2024-12-04 23:56:26 -0600

    Initial commit

---
+++ KerbalWilds/KerbalWilds.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using OWML.Common;
+using OWML.ModHelper;
+using System.Reflection;
+
+namespace KerbalWilds
+{
+    public class KerbalWilds : ModBehaviour
+    {
+        public static KerbalWilds Instance;
+        public INewHorizons NewHorizons;
+
+        public void Awake()
+        {
+            Instance = this;
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+        }
+
+        public void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(KerbalWilds)} is loaded!", MessageType.Success);
+
+            // Get the New Horizons API and load configs
+            NewHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            NewHorizons.LoadConfigs(this);
+
+            new Harmony("Trifid.KerbalWilds").PatchAll(Assembly.GetExecutingAssembly());
+
+            // Example of accessing game code.
+            OnCompleteSceneLoad(OWScene.TitleScreen, OWScene.TitleScreen); // We start on title screen
+            LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
+        }
+
+        public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
+        {
+            if (newScene != OWScene.SolarSystem) return;
+            ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+        }
+    }
+}
