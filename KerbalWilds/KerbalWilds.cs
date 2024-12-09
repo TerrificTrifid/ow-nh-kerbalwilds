92470d89aafddc1b0b425a6ad6b36b141f2150f8
TerrificTrifid <99054745+TerrificTrifid@users.noreply.github.com>
2024-12-09 15:53:58 -0600

    start custom stuff

--- KerbalWilds/KerbalWilds.cs
+++ KerbalWilds/KerbalWilds.cs
@@ -35,18 +35,52 @@ namespace KerbalWilds
                 if (system != "Kerbol System") return;
 
                 var moho = NewHorizons.GetPlanet("Moho");
-                moho.transform.Find("GravityWell").GetComponent<GravityVolume>()._alignmentPriority = 1;
-
+                var eve = NewHorizons.GetPlanet("Eve");
+                var gilly = NewHorizons.GetPlanet("Gilly");
+                var kerbin = NewHorizons.GetPlanet("Kerbin");
+                var mun = NewHorizons.GetPlanet("The Mun");
+                var minmus = NewHorizons.GetPlanet("Minmus");
                 var duna = NewHorizons.GetPlanet("Duna");
                 var ike = NewHorizons.GetPlanet("Ike");
-                var align = duna.GetAddComponent<AlignWithTargetBody>();
-                align.SetTargetBody(ike.GetAttachedOWRigidbody());
-                align._localAlignmentAxis = new Vector3(0,0,-1);
-                align._usePhysicsToRotate = true;
+                var dres = NewHorizons.GetPlanet("Dres");
+                var jool = NewHorizons.GetPlanet("Jool");
+                var laythe = NewHorizons.GetPlanet("Laythe");
+                var vall = NewHorizons.GetPlanet("Vall");
+                var tylo = NewHorizons.GetPlanet("Tylo");
+                var bop = NewHorizons.GetPlanet("Bop");
+                var pol = NewHorizons.GetPlanet("Pol");
+                var eeloo = NewHorizons.GetPlanet("Eeloo");
 
-                var kerbin = NewHorizons.GetPlanet("Kerbin");
+                moho.transform.Find("GravityWell").GetComponent<GravityVolume>()._alignmentPriority = 1;
+
                 kerbin.transform.Find("AmbientLight_TH").GetComponent<Light>().intensity = 1;
+                
+                var dunaAlign = duna.GetAddComponent<AlignWithTargetBody>();
+                dunaAlign.SetTargetBody(ike.GetAttachedOWRigidbody());
+                dunaAlign._localAlignmentAxis = new Vector3(0,0,-1);
+                dunaAlign._usePhysicsToRotate = true;
+
+                SetupGreenMonolith(ike);
+
+                SetupGreenMonolith(dres);
+
+
             });
+        }
+
+        private void SetupGreenMonolith(GameObject body)
+        {
+            try
+            {
+                var recorder = body.transform.Find("Sector/GreenMonolith/Prefab_NOM_Recorder");
+                recorder.Find("Props_NOM_Recorder").gameObject.SetActive(false);
+                recorder.Find("PointLight_NOM_Recorder").gameObject.SetActive(false);
+                recorder.Find("Audio_Recorder").gameObject.SetActive(false);
+            }
+            catch (System.Exception)
+            {
+                ModHelper.Console.WriteLine($"Failed to set up monolith at {body.name}", MessageType.Error);
+            }
         }
     }
 }
