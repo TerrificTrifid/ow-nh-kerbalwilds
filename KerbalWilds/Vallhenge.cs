5023e25580b722f9384acbbdb8aed9e07f133c7b
TerrificTrifid <99054745+TerrificTrifid@users.noreply.github.com>
2025-01-08 20:28:30 -0600

    fix animations

--- KerbalWilds/Vallhenge.cs
+++ KerbalWilds/Vallhenge.cs
@@ -7,10 +7,10 @@ namespace KerbalWilds
 {
     public class Vallhenge : MonoBehaviour
     {
-        public static float RiseTime = 2f;
+        public static float RiseTime = 3f;
         public static float RotateTime = 4f;
         public static float OrbitSpeed = 180f;
-        public static float OrbitSpeedFalloff = 5f;
+        public static float OrbitSpeedFalloff = 10f;
         public static Color EmissionColor = new Color(0.5f, 0f, 1f);
         
         public MeshRenderer[] EmissionRenderers;
@@ -20,7 +20,8 @@ namespace KerbalWilds
         public Transform[] OctahedronPivots;
 
         private bool _isActivated = false;
-        private bool _isRising = false;
+        private bool _isOpen = false;
+        private bool _isClosed = false;
         private float _rise = 0f;
         private float _rotate = 0f;
 
@@ -31,26 +32,43 @@ namespace KerbalWilds
 
         public void Start()
         {
+            for (int i = 0; i < EmissionRenderers.Length; i++)
+            {
+                EmissionRenderers[i].material.SetColor("_EmissionColor", Color.black);
+            }
+            for (int i = 0; i < Lights.Length; i++)
+            {
+                Lights[i].intensity = 0f;
+            }
+
             for (int i = 0; i < PyramidAnimators.Length; i++)
             {
-                PyramidAnimators[i].speed = -1f / RiseTime;
+                PyramidAnimators[i].speed = 1f / RiseTime;
             }
-            OctahedronAnimator.speed = -1f / RiseTime;
+            OctahedronAnimator.speed = 1f / RiseTime;
+            ClosePyramids();
+            _isClosed = true;
         }
 
         public void Update()
         {
             _isActivated = DialogueConditionManager.SharedInstance.GetConditionState("ActivateVallhenge");
 
-            var change = false;
+            var visualChange = false;
 
             if (_isActivated)
             {
                 if (_rise < RiseTime)
                 {
                     _rise = Mathf.Min(RiseTime, _rise + Time.deltaTime);
-                    _isRising = true;
-                    change = true;
+                    visualChange = true;
+
+                    _isClosed = false;
+                    if (!_isOpen)
+                    {
+                        OpenPyramids();
+                        _isOpen = true;
+                    }
                 }
                 else if (_rotate < RotateTime)
                 {
@@ -66,49 +84,34 @@ namespace KerbalWilds
                 else if (_rise > 0f)
                 {
                     _rise = Mathf.Max(0f, _rise - Time.deltaTime);
-                    _isRising = false;
-                    change = true;
+                    visualChange = true;
+
+                    _isOpen = false;
+                    if (!_isClosed)
+                    {
+                        ClosePyramids();
+                        _isClosed = true;
+                    }
                 }
             }
 
-            if (change)
+            if (visualChange)
             {
-                ApplyChange();
+                ApplyVisualChange();
             }
 
             for (int i = 0; i < OctahedronPivots.Length; i++)
             {
                 var angle = OctahedronPivots[i].localRotation.eulerAngles;
-                var orbit = Time.time * OrbitSpeed / ((i + 1) * OrbitSpeedFalloff);
+                var orbit = (Time.time - _rotate) * OrbitSpeed / ((i + 1) * OrbitSpeedFalloff);
                 angle.y = Mathf.LerpAngle(0f, orbit, _rotate / RotateTime);
                 OctahedronPivots[i].localRotation = Quaternion.Euler(angle);
             }
         }
 
-        public void ApplyChange()
+        public void ApplyVisualChange()
         {
             var rise = _rise / RiseTime;
-
-            if (_isRising)
-            {
-                for (int i = 0; i < PyramidAnimators.Length; i++)
-                {
-                    PyramidAnimators[i].SetTrigger("Open");
-                    PyramidAnimators[i].ResetTrigger("Close");
-                }
-                OctahedronAnimator.SetTrigger("Rise");
-                OctahedronAnimator.ResetTrigger("Lower");
-            }
-            else
-            {
-                for (int i = 0; i < PyramidAnimators.Length; i++)
-                {
-                    PyramidAnimators[i].SetTrigger("Close");
-                    PyramidAnimators[i].ResetTrigger("Open");
-                }
-                OctahedronAnimator.SetTrigger("Lower");
-                OctahedronAnimator.ResetTrigger("Rise");
-            }
 
             for (int i = 0; i < EmissionRenderers.Length; i++)
             {
@@ -118,6 +121,28 @@ namespace KerbalWilds
             {
                 Lights[i].intensity = rise;
             }
+        }
+
+        public void OpenPyramids()
+        {
+            for (int i = 0; i < PyramidAnimators.Length; i++)
+            {
+                PyramidAnimators[i].SetTrigger("Open");
+                PyramidAnimators[i].ResetTrigger("Close");
+            }
+            OctahedronAnimator.SetTrigger("Rise");
+            OctahedronAnimator.ResetTrigger("Lower");
+        }
+
+        public void ClosePyramids()
+        {
+            for (int i = 0; i < PyramidAnimators.Length; i++)
+            {
+                PyramidAnimators[i].SetTrigger("Close");
+                PyramidAnimators[i].ResetTrigger("Open");
+            }
+            OctahedronAnimator.SetTrigger("Lower");
+            OctahedronAnimator.ResetTrigger("Rise");
         }
     }
 }
